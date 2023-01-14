fd95909953bcbc148cc99220ccabb0684b27f284
Eugene <klev.evzen@gmail.com>
2023-01-14 20:01:48 +0100

    added slices for  car s and form

---
+++ src/store/slices/formSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const formSlice = createSlice({
+  name: "form",
+  initialState: {
+    name: "",
+    cost: 0,
+  },
+  reducers: {
+    changeName(state, action) {
+      state.name = action.payload;
+    },
+    changeCost(state, action) {
+      state.cost = action.payload;
+    },
+  },
+});
+
+// export actions from slice object
+export const { changeCost, changeName } = formSlice.actions;
+// export reducer from slice object
+export const formReducer = formSlice.reducer;
