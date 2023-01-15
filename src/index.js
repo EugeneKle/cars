d2c79d2efdf3fbf8bee72fa1559f80eb51da273e
Eugene <klev.evzen@gmail.com>
2023-01-15 22:40:04 +0100

    already done input form and car list

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import "./styles.css";
 
 const element = document.getElementById("root");
 
 const root = createRoot(element);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
